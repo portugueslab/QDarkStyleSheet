98d4632ca1570b8497d7452b5ceec53a0572f8b9
Daniel Pizetta <daniel.pizetta@usp.br>
2018-04-27 15:28:48 -0300

    Fix more things and add reset argument to cli to new version

--- example/example.py
+++ example/example.py
@@ -57,6 +57,7 @@ from os.path import abspath, dirname
 
 # show all warnings
 warnings.simplefilter('always')
+
 # make the example runnable without the need to install
 sys.path.insert(0, abspath(dirname(abspath(__file__)) + '/..'))
 
@@ -72,6 +73,8 @@ def main():
                         help="Exihibts the original  window (without qdarkstyle).")
     parser.add_argument('--test', action='store_true',
                         help="Auto close window after 2s.")
+    parser.add_argument('--reset', action='store_true',
+                        help="Reset stored application settings.")
 
     # parsing arguments from command line
     args = parser.parse_args()
@@ -80,14 +83,7 @@ def main():
     os.environ['QT_API'] = args.qt_api
 
     # using QtPy API
-    import qtpy
-    from qtpy.QtWidgets import QApplication, QMainWindow, QDockWidget
-    from qtpy.QtCore import QTimer, Qt, QSettings
-
-    # test if it using choosen binding
-    if args.qt_api != os.environ['QT_API']:
-        warnings.warn("Qt binding '{}' not found. Using '{}' instead.".format(
-                      args.qt_api, os.environ['QT_API']), RuntimeWarning)
+    from qtpy import QtGui, QtWidgets, QtCore, API_NAME
 
     # import examples UI
     from ui.mw_menus_ui import Ui_MainWindow as ui_main
@@ -112,23 +108,28 @@ def main():
     # functions to restore window state after closing
     def write_settings(window):
         """Get window settings and write it into a file."""
-        settings = QSettings('QDarkStyle', 'QDarkStyle Example')
+        settings = QtCore.QSettings('QDarkStyle', 'QDarkStyle Example')
         settings.setValue('pos', window.pos())
         settings.setValue('size', window.size())
         settings.setValue('state', window.saveState())
 
-    def read_settings(window):
+    def read_settings(window, reset=False):
         """Read and set window settings from a file."""
-        settings = QSettings('QDarkStyle', 'QDarkStyle Example')
+        settings = QtCore.QSettings('QDarkStyle', 'QDarkStyle Example')
+
+        if reset:
+            settings.clear()
+
         pos = settings.value('pos', window.pos())
         size = settings.value('size', window.size())
         state = settings.value('state', window.saveState())
+
         window.restoreState(state)
         window.resize(size)
         window.move(pos)
 
     # create the application
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     app.setOrganizationName('QDarkStyle')
     app.setApplicationName('QDarkStyle Example')
 
@@ -136,81 +137,90 @@ def main():
     app.setStyleSheet(style)
 
     # create main window
-    window = QMainWindow()
+    window = QtWidgets.QMainWindow()
     window.setObjectName('mainwindow')
     ui = ui_main()
     ui.setupUi(window)
     window.setWindowTitle("QDarkStyle v." + qdarkstyle.__version__ +
-                          " - Example - Using QtPy with " + qtpy.API_NAME)
+                          " - Example - Using QtPy with " + API_NAME)
 
     # create docks for buttons
-    dw_buttons = QDockWidget()
+    dw_buttons = QtWidgets.QDockWidget()
     dw_buttons.setObjectName('buttons')
     ui_buttons = ui_buttons()
     ui_buttons.setupUi(dw_buttons)
-    window.addDockWidget(Qt.RightDockWidgetArea, dw_buttons)
+    window.addDockWidget(QtCore.Qt.RightDockWidgetArea, dw_buttons)
 
     # create docks for buttons
-    dw_displays = QDockWidget()
+    dw_displays = QtWidgets.QDockWidget()
     dw_displays.setObjectName('displays')
     ui_displays = ui_displays()
     ui_displays.setupUi(dw_displays)
-    window.addDockWidget(Qt.RightDockWidgetArea, dw_displays)
+    window.addDockWidget(QtCore.Qt.RightDockWidgetArea, dw_displays)
 
     # create docks for inputs - fields
-    dw_inputs_fields = QDockWidget()
+    dw_inputs_fields = QtWidgets.QDockWidget()
     dw_inputs_fields.setObjectName('fields')
     ui_inputs_fields = ui_inputs_fields()
     ui_inputs_fields.setupUi(dw_inputs_fields)
-    window.addDockWidget(Qt.RightDockWidgetArea, dw_inputs_fields)
+    window.addDockWidget(QtCore.Qt.RightDockWidgetArea, dw_inputs_fields)
 
     # create docks for inputs - no fields
-    dw_inputs_no_fields = QDockWidget()
+    dw_inputs_no_fields = QtWidgets.QDockWidget()
     dw_inputs_no_fields.setObjectName('inputs_no_fields')
     ui_inputs_no_fields = ui_inputs_no_fields()
     ui_inputs_no_fields.setupUi(dw_inputs_no_fields)
-    window.addDockWidget(Qt.RightDockWidgetArea, dw_inputs_no_fields)
+    window.addDockWidget(QtCore.Qt.RightDockWidgetArea, dw_inputs_no_fields)
 
-    # create docks for widgets
-    dw_widgets = QDockWidget()
+    # create docks for widgets - QListWidget
+    dw_widgets = QtWidgets.QDockWidget()
     dw_widgets.setObjectName('widgets')
     ui_widgets = ui_widgets()
     ui_widgets.setupUi(dw_widgets)
-    window.addDockWidget(Qt.LeftDockWidgetArea, dw_widgets)
+    window.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dw_widgets)
 
     # create docks for views
-    dw_views = QDockWidget()
+    dw_views = QtWidgets.QDockWidget()
     dw_views.setObjectName('views')
     ui_views = ui_views()
     ui_views.setupUi(dw_views)
-    window.addDockWidget(Qt.LeftDockWidgetArea, dw_views)
+    window.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dw_views)
 
     # create docks for containters - tabs
-    dw_containers_tabs = QDockWidget()
+    dw_containers_tabs = QtWidgets.QDockWidget()
     dw_containers_tabs.setObjectName('containers')
     ui_containers_tabs = ui_containers_tabs()
     ui_containers_tabs.setupUi(dw_containers_tabs)
-    window.addDockWidget(Qt.LeftDockWidgetArea, dw_containers_tabs)
+    window.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dw_containers_tabs)
 
     # create docks for containers - no tabs
-    dw_containers_no_tabs = QDockWidget()
+    dw_containers_no_tabs = QtWidgets.QDockWidget()
     dw_containers_no_tabs.setObjectName('containers_no_tabs')
     ui_containers_no_tabs = ui_containers_no_tabs()
     ui_containers_no_tabs.setupUi(dw_containers_no_tabs)
-    window.addDockWidget(Qt.LeftDockWidgetArea, dw_containers_no_tabs)
+    window.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dw_containers_no_tabs)
 
     # tabify right docks
     window.tabifyDockWidget(dw_buttons, dw_displays)
     window.tabifyDockWidget(dw_displays, dw_inputs_fields)
     window.tabifyDockWidget(dw_inputs_fields, dw_inputs_no_fields)
 
+    # tabify left docks
+    window.tabifyDockWidget(dw_widgets, dw_views)
+    window.tabifyDockWidget(dw_views, dw_containers_tabs)
+    window.tabifyDockWidget(dw_containers_tabs, dw_containers_no_tabs)
+
     # auto quit after 2s when testing on travis-ci
     if "--test" in sys.argv:
-        QTimer.singleShot(2000, app.exit)
+        QtCore.QTimer.singleShot(2000, app.exit)
 
     # run
-    read_settings(window)
+    read_settings(window, args.reset)
     window.showMaximized()
+    window.setWindowFlags(window.windowFlags() |
+                          QtCore.Qt.WindowMinimizeButtonHint |
+                          QtCore.Qt.WindowMaximizeButtonHint |
+                          QtCore.Qt.WindowCloseButtonHint)
     app.exec_()
     write_settings(window)
 
